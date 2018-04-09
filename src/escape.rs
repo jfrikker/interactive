5b3532a1ff7686b486d0c9ae9627dd73fce04a83
Joe Frikker <jfrikker@gmail.com>
2018-04-08 20:58:32 -0400

    escape tests

--- src/escape.rs
+++ src/escape.rs
@@ -56,3 +56,78 @@ pub fn split_command(line: &str) -> Vec<&str> {
 
     result
 }
+
+#[cfg(test)]
+mod tests {
+    use super::split_command;
+
+    #[test]
+    fn empty() {
+        assert_eq!(split_command(""), Vec::<&str>::new());
+    }
+
+    #[test]
+    fn just_spaces() {
+        assert_eq!(split_command("  "), Vec::<&str>::new());
+    }
+
+    #[test]
+    fn single_string() {
+        assert_eq!(split_command("abc"), vec!("abc"));
+    }
+
+    #[test]
+    fn leading_space() {
+        assert_eq!(split_command(" abc"), vec!("abc"));
+    }
+
+    #[test]
+    fn trailing_space() {
+        assert_eq!(split_command("abc "), vec!("abc"));
+    }
+
+    #[test]
+    fn two_args() {
+        assert_eq!(split_command("abc def"), vec!("abc", "def"));
+    }
+
+    #[test]
+    fn two_args_more_space() {
+        assert_eq!(split_command("abc    def"), vec!("abc", "def"));
+    }
+
+    #[test]
+    fn single_quote() {
+        assert_eq!(split_command("one 'two three' four"), vec!("one", "two three", "four"));
+    }
+
+    #[test]
+    fn double_quote() {
+        assert_eq!(split_command("one \"two three\" four"), vec!("one", "two three", "four"));
+    }
+
+    #[test]
+    fn double_in_single() {
+        assert_eq!(split_command("one 'two\"three' four"), vec!("one", "two\"three", "four"));
+    }
+
+    #[test]
+    fn single_in_double() {
+        assert_eq!(split_command("one \"two'three\" four"), vec!("one", "two'three", "four"));
+    }
+
+    #[test]
+    fn dangling_single() {
+        assert_eq!(split_command("one 'two three"), vec!("one", "two three"));
+    }
+
+    #[test]
+    fn dangling_double() {
+        assert_eq!(split_command("one \"two three"), vec!("one", "two three"));
+    }
+
+    #[test]
+    fn adjacent_quotes() {
+        assert_eq!(split_command("one'two'\"three\"'four five'"), vec!("one", "two", "three", "four five"));
+    }
+}
